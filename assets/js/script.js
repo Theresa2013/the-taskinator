70f208365051cbd00a1f72c5bae0868e03d54309
Your Name <you@example.com>
2022-04-20 11:28:11 -0500

    form submission updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,13 @@
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1);
+var formE1 = document.querySelector("#task-form");
+var tasksToDoE1 = document.querySelector("#tasks-to-do");
+
+var createTaskHandler = function (event) {
+    event.preventDefault();
+
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoE1.appendChild(listItemEl);
+};
+
+formE1.addEventListener("submit", createTaskHandler);
